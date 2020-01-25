139c1a05a679a9a0b45495d1d9660495b283a404
Muhammed Gaber <mohammedgaber273@gmail.com>
2020-01-25 16:35:10 +0200

    HTTPS

--- app.js
+++ app.js
@@ -36,6 +36,18 @@ connect.then(
 // express inistance
 var app = express();
 
+// middleware for handling unsecure requests
+app.all("*", (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    res.redirect(
+      307,
+      `https://${req.hostname}:${app.get("secPort")}${req.url}`
+    );
+  }
+});
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
